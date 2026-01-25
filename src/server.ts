ea12eaab02fab909f7f1259afad9ce648fb3b58b
Subham Khadka <subhamkhadka99@gmail.com>
2026-01-25 15:46:12 +0545

    express server created

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,22 @@
 // npm init -y
 // npx tsc --init
 
+import express, { Request, Response } from "express";
+const app = express();
+const PORT =8000
+//! connect to database
+
+
+//! root route
+app.get("/",(req:Request ,res:Response)=>{
+    res.status(200).json({
+        message: "server is up and running"
+    })
+})
+
+
+
+// listen
+app.listen(PORT , ()=>{
+    console.log(`server is running at http://localhost:${PORT}`);
+})
